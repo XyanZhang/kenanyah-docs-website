fa25b71e36323776e27b3aad7cccb9cdd6040b92
XyanZhang <1743067175@qq.com>
2023-04-14 11:32:06 +0800

    chore: 接入本地搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,6 +134,24 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
+  // plugins: [
+  //   require.resolve("@cmfcmf/docusaurus-search-local")
+  // ],
 };
 
 module.exports = config;
