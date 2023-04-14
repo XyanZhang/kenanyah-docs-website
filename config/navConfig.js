7ed1d3ab0906534a62f6fbde31da8b981e1e136b
XyanZhang <1743067175@qq.com>
2023-04-14 17:02:16 +0800

    feat: navConfig

---
+++ config/navConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'Kenanyah Site',
+  logo: {
+    alt: 'My Site Logo',
+    src: 'img/logo.svg',
+  },
+  items: [
+    {
+      type: 'docSidebar',
+      sidebarId: 'tutorialSidebar',
+      position: 'right',
+      label: 'Tutorial',
+    },
+    {to: '/blog', label: 'Blog', position: 'right'},
+  ],
+}
